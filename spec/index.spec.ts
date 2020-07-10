1d2430bf06797202d6b34b6fd83276c81e5b98fb
hansa-lallu <hansalallu@gmail.com>
2020-07-10 10:31:29 +0100

    chai setup

---
+++ spec/index.spec.ts
@@ -0,0 +1,8 @@
+// import { Options } from '../src/Classes/Options/Options'; // this will be your custom import
+import { expect } from 'chai';
+
+describe('Options tests', () => { // the tests container
+    it('checking default options', () => { // the single test
+        expect(1).to.equal(1); // Do I need to explain anything? It's like writing in English!
+    });
+});
